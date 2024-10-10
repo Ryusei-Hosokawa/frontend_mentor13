6816472d9f55cc266b3194913f050fa3b193ff7f
Ryusei-Hosokawa <korokoro.seika@gmail.com>
2024-10-10 21:37:32 +0900

    10/10 add header

--- src/MainContents.tsx
+++ src/MainContents.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import "./css/MainContents.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Collections from "./pages/Collections.tsx";
-import Men from "./pages/Men.tsx";
-import Women from "./pages/Women.tsx";
-import About from "./pages/About.tsx";
-import Contact from "./pages/Contact.tsx";
+import "./css/MainContents.css";
 
 export default function MainContents() {
     return (
         <main className="main-contents">
             <Routes>
+                <Route path="/" element={<div className="no-contents">NO CONTENTS</div>} />
                 <Route path="/collections" element={<Collections />} />
-                <Route path="" element={<Men />} />
-                <Route path="" element={<Women />} />
-                <Route path="" element={<About />} />
-                <Route path="" element={<Contact />} />
             </Routes>
         </main>
     );
